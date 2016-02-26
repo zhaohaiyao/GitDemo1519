9531526588c9a358ab0e14c1718ab670b3cfe041
zhaohaiyao <953127192@qq.com>
2016-02-26 14:25:35 +0800

    work2创建一个sudent 类

---
+++ gitDemo/gitDemo/student.m
@@ -0,0 +1,13 @@
+//
+//  student.m
+//  gitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 赵海耀. All rights reserved.
+//
+
+#import "student.h"
+
+@implementation student
+
+@end
