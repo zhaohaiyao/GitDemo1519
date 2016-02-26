aa9c56be43f24be7b569b13597ecb5b40411b8ad
zhaohaiyao <953127192@qq.com>
2016-02-26 14:20:15 +0800

    work1 创建一个person类

---
+++ gitDemo/gitDemo/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 赵海耀. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ gitDemo/gitDemo/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  gitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 赵海耀. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
