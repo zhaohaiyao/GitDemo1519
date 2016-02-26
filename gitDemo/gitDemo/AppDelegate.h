7f33a624c9f0d60a24fabe009949b3aa6b3147db
zhaohaiyao <953127192@qq.com>
2016-02-26 14:08:25 +0800

    first commit

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 赵海耀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/gitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 赵海耀. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
